c9c1a00996ad6bc9f3f41c3bc8ec53a7f80c7c73
Mike Goren <mgoren@mortalwombat.net>
2015-03-14 17:08:35 -0700

    add web development section to site

--- router.js
+++ router.js
@@ -2,4 +2,5 @@ app.Router.map(function() {
   this.resource('galleries', {path: '/'});
   this.resource('gallery', {path:'/galleries/:gallery_id'});
   this.resource('about');
+  this.resource('web');
 });
